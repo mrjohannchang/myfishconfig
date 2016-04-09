2ba50847c3976316e8e37832d1e10910c70ee60f
Chang Yu-heng <mr.changyuheng@gmail.com>
2016-04-08 18:02:04 -0700

    Support gpg-agent

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-[![Build Status][travis-badge]][travis-link]
-[![Slack Room][slack-badge]][slack-link]
-
-# changyuheng-fish-config
+# My Fish Config
 
 My personal fish config
 
@@ -10,11 +7,7 @@ My personal fish config
 With [Fisherman]
 
 ```
-fisher i changyuheng/changyuheng-fish-config
+fisher i changyuheng/myfishconfig
 ```
 
-[travis-link]: https://travis-ci.org/changyuheng/changyuheng-fish-config
-[travis-badge]: https://img.shields.io/travis/changyuheng/changyuheng-fish-config.svg?style=flat-square
-[slack-link]: https://fisherman-wharf.herokuapp.com/
-[slack-badge]: https://img.shields.io/badge/slack-join%20the%20chat-00B9FF.svg?style=flat-square
 [Fisherman]: https://github.com/fisherman/fisherman
--- functions/myfishconfig.fish
+++ functions/myfishconfig.fish
@@ -67,3 +67,58 @@ else if [ $JAVA7_HOME ]
     set -gx JAVA_HOME $JAVA7_HOME
 end
 # }}}
+
+# gpg-agent {{{
+function __get_gpg_agent_sock
+  set --local __gpg_agent_info_file "$argv[1]"
+  cat "$__gpg_agent_info_file" | while read --local line
+    if echo -n "$line" | grep -q "S\.gpg-agent" > /dev/null 2>&1
+      echo -n "$line" | sed 's/.*\(\/tmp\/gpg.*\/S\.gpg-agent\).*/\1/'
+      break
+    end
+  end
+end
+
+function __get_gpg_agent_info
+  set --local __gpg_agent_info_file "$argv[1]"
+  cat "$__gpg_agent_info_file" | while read --local line
+    if echo -n "$line" | grep -q "S\.gpg-agent" > /dev/null 2>&1
+      echo -n "$line" | sed 's/GPG_AGENT_INFO=//' | sed 's/; export GPG_AGENT_INFO;//'
+      break
+    end
+  end
+end
+
+set __gpg_agent_info_file "$HOME/.gpg-agent-info"
+
+if test -f "$__gpg_agent_info_file"
+  set --local __gpg_agent_info (__get_gpg_agent_info "$__gpg_agent_info_file")
+  set --local __gpg_agent_sock (__get_gpg_agent_sock "$__gpg_agent_info_file")
+  if test -S "$__gpg_agent_sock"
+    set --export --global GPG_AGENT_INFO "$__gpg_agent_info"
+  else
+    set --erase --unexport GPG_AGENT_INFO
+  end
+else
+  if test -n "$GPG_AGENT_INFO"
+    set --local __gpg_agent_sock (__get_gpg_agent_sock <(echo -n $GPG_AGENT_INFO))
+    if not test -S "$__gpg_agent_sock"
+      set --erase --unexport GPG_AGENT_INFO
+    end
+  end
+end
+
+if test -z "$GPG_AGENT_INFO"
+  rm -rf "$__gpg_agent_info_file"
+  pgrep -U (id -u) "gpg-agent" | while read --local line
+    kill "$line"
+  end
+  command -v gpg-agent >/dev/null 2>&1 \
+    ; and gpg-agent --daemon --sh --default-cache-ttl 7200 > "$__gpg_agent_info_file"
+  set --export --global GPG_AGENT_INFO (__get_gpg_agent_info "$__gpg_agent_info_file")
+end
+
+set --erase __gpg_agent_info_file
+functions --erase __get_gpg_agent_sock
+functions --erase __get_gpg_agent_info
+# }}}
